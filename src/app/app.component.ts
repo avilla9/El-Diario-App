c4386d6a0cf9fb2ef3e799686ea780d2563e5438
Rafael  Gomez <rgomez@contractors.ondiversity.eu>
2023-03-07 18:22:38 -0400

    [UPD] padding top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PushNotificationService } from './services/push-notification.service';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,20 @@ import { PushNotificationService } from './services/push-notification.service';
 export class AppComponent {
 
   public href: string = window.location.pathname;
-
+ 
   constructor(
     private router: Router,
+    private platform: Platform,
     public pushNotifications: PushNotificationService,
   ) {
     this.pushNotifications.initPush();
+  }
+    initializeApp() {
+      this.platform.ready().then( async () => {
+      // set status bar to white
+      await StatusBar.setStyle({style:Style.Dark});
+      // set status bar to white
+    });
   }
 
 }
