e6e23a927de97802f7dadd63bcaf11876be6cd69
Armando Villanueva <avillanueva@ext.diversity.gg>
2023-03-09 02:57:25 -0400

    update enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gallant-solomon.82-223-203-9.plesk.page/api',
-  authUrl: 'https://gallant-solomon.82-223-203-9.plesk.page/oauth/token',
+  apiUrl: 'https://ecstatic-mayer.82-223-203-9.plesk.page/api',
+  authUrl: 'https://ecstatic-mayer.82-223-203-9.plesk.page/oauth/token',
   client_secret: '72Rsucj8E9qDYhfDJMnOictQLwnR1ITsN0FGZaV7',
   client_id: '2'
 };
